14347141934a72cd3d3b62d5299a4801960fca81
Daniel Avila <danielsavila2020@gmail.com>
2025-09-01 11:53:32 -0500

    finished bias-variance graph

--- bias-variance.py
+++ bias-variance.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-x_val = np.linspace(0, 6, 100, dtype = float)
+complexity = np.linspace(0, 6, 100, dtype = float)
 
 # bias
 '''
@@ -10,23 +10,37 @@ bias: the difference between the predicted values of a model and the correct val
 
 i.e. high bias models are inflexible models (think linear regression)
 '''
-bias_y_val = (1 / x_val ** 3) + 1
-
+bias_y_val = (1/np.exp(complexity)) * 11
+              
 
 # variance 
 '''
 variance: the variability of a model prediction for a given data point
 
-i.e. high variability models are extremely flexible models (think polynomial regression)
+i.e. high variability models are more flexible models (think polynomial regression)
 '''
-
+variance_y_val = np.exp(complexity) / 36
 
 # test error
-''' test error:'''
-y_val_test_error = ((x_val -3) ** 2) + 2
+''' test error: the difference between the true value in the test set and the value that 
+we predicted in the test set.'''
+y_val_test_error = ((complexity -3) ** 2) + 2
 
 plt.axis(ymax = 10, ymin = 0, xmax = 6, xmin = 0)
-plt.plot(x_val, y_val_test_error, "--", label = "test error")
-plt.plot(x_val, bias_y_val, "-", label = "bias squared")
+plt.plot(complexity, y_val_test_error, "--", label = "test error")
+plt.plot(complexity, bias_y_val, ".", label = "bias squared")
+plt.plot(complexity, variance_y_val, ".", label = "variance")
 plt.legend()
+plt.xlabel("Complexity")
+plt.ylabel("Error")
 plt.show()
+
+
+'''
+the main lessons here are that 
+1) bias means low flexibilty model
+2) variance means high flexibility model
+3) bias variance tradeoff is that as we more from more biased to more flexible models,
+we get decreasing, then increasing test error. i.e. our predictions get better to a point,
+but then after that point our test predictions get worse.
+'''
